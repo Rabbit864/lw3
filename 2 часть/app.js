19a728b88b5992657635cf41949f59af831c484e
Daniil Smyshlyaev <smyshlyeavar@gmail.com>
2020-11-17 19:26:42 +0300

    feat: added button add element in the listing

--- 2 часть/app.js
+++ 2 часть/app.js
@@ -1,8 +1,7 @@
-window.onload = function () {
-
-  var listingElements = ['apple', 'orange'];
+window.onload = function() {
+  var listingElements = ["apple", "orange"];
   var storeElements = [];
- 
+
   // логика JS, не связана с DOM
   function addToStoreElements(element) {
     var elementPosition = listingElements.indexOf(element);
@@ -20,53 +19,69 @@ window.onload = function () {
     }
   }
 
-  function deleteListElement(element){
+  function deleteListElement(element) {
     var elementPosition = listingElements.indexOf(element);
     if (elementPosition > -1) {
       listingElements.splice(elementPosition, 1);
     }
   }
- 
+
+  function addElementToList(element) {
+    listingElements.push(element);
+  }
+
   // updateUI берет данные из массивов и занимается вставкой
   function updateUI() {
-    var storeSelect = document.querySelector('.store-select');
-    var listingSelect = document.querySelector('.listing-select');
-    storeSelect.innerHTML = '';
-    listingSelect.innerHTML = '';
-    
+    var storeSelect = document.querySelector(".store-select");
+    var listingSelect = document.querySelector(".listing-select");
+    storeSelect.innerHTML = "";
+    listingSelect.innerHTML = "";
+    console.log(listingElements);
     for (var i = 0; i < listingElements.length; i++) {
-      var newOption = document.createElement('option');
+      var newOption = document.createElement("option");
       newOption.innerText = listingElements[i];
       listingSelect.append(newOption);
     }
-    
+
     for (var i = 0; i < storeElements.length; i++) {
-      var newOption = document.createElement('option');
+      var newOption = document.createElement("option");
       newOption.innerText = storeElements[i];
       storeSelect.append(newOption);
     }
   }
 
   // регистрируем события
-  var addButton = document.querySelector('#add-button-store');
-  var addButton1 = document.querySelector('#add-button-list');
-  var deleteButton = document.querySelector('#delete-button-list');
+  var addButton = document.querySelector("#add-button-store");
+  var addButton1 = document.querySelector("#add-button-list");
+  var deleteButton = document.querySelector("#delete-button-list");
+  var addElementButton = document.querySelector("#add-elemetn-list-button");
 
-  addButton.onclick = function () {
-    var selectedOption = document.querySelector('.listing-select option:checked');
+  addButton.onclick = function() {
+    var selectedOption = document.querySelector(
+      ".listing-select option:checked"
+    );
     addToStoreElements(selectedOption.innerText);
     updateUI();
-  }
+  };
 
-  addButton1.onclick = function () {
-    var selectedOption = document.querySelector('.store-select option:checked');
+  addButton1.onclick = function() {
+    var selectedOption = document.querySelector(".store-select option:checked");
     addToListElements(selectedOption.innerText);
     updateUI();
-  }
+  };
 
-  deleteButton.onclick = function(){
-    var selectedOption = document.querySelector('.listing-select option:checked');
+  deleteButton.onclick = function() {
+    var selectedOption = document.querySelector(
+      ".listing-select option:checked"
+    );
     deleteListElement(selectedOption.innerText);
     updateUI();
-  }
+  };
+  addElementButton.onclick = function() {
+    var result = prompt('Введите элемент');
+    if(result !== ''){
+      addElementToList(result);
+      updateUI();
+    }
+  };
 };
