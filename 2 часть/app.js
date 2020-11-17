06644091e4968af32ea8cf9abf8e8e03771adaa1
Daniil Smyshlyaev <smyshlyeavar@gmail.com>
2020-11-17 17:41:04 +0300

    feat: added add to list

---
+++ 2 часть/app.js
@@ -0,0 +1,58 @@
+window.onload = function () {
+
+  var listingElements = ['apple', 'orange'];
+  var storeElements = [];
+ 
+  // логика JS, не связана с DOM
+  function addToStoreElements(element) {
+    var elementPosition = listingElements.indexOf(element);
+    if (elementPosition > -1) {
+      storeElements.push(element);
+      listingElements.splice(elementPosition, 1);
+    }
+  }
+
+  function addToListElements(element) {
+    var elementPosition = storeElements.indexOf(element);
+    if (elementPosition > -1) {
+      listingElements.push(element);
+      storeElements.splice(elementPosition, 1);
+    }
+  }
+ 
+  // updateUI берет данные из массивов и занимается вставкой
+  function updateUI() {
+    var storeSelect = document.querySelector('.store-select');
+    var listingSelect = document.querySelector('.listing-select');
+    storeSelect.innerHTML = '';
+    listingSelect.innerHTML = '';
+    
+    for (var i = 0; i < listingElements.length; i++) {
+      var newOption = document.createElement('option');
+      newOption.innerText = listingElements[i];
+      listingSelect.append(newOption);
+    }
+    
+    for (var i = 0; i < storeElements.length; i++) {
+      var newOption = document.createElement('option');
+      newOption.innerText = storeElements[i];
+      storeSelect.append(newOption);
+    }
+  }
+
+  // регистрируем события
+  var addButton = document.querySelector('#add-button-store');
+  var addButton1 = document.querySelector('#add-button-list');
+
+  addButton.onclick = function () {
+    var selectedOption = document.querySelector('.listing-select option:checked');
+    addToStoreElements(selectedOption.innerText);
+    updateUI();
+  }
+
+  addButton1.onclick = function () {
+    var selectedOption = document.querySelector('.store-select option:checked');
+    addToListElements(selectedOption.innerText);
+    updateUI();
+  }
+};
